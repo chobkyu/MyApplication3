eb3d45d93530d3543f17a6b02107ee132f662b4c
chobkyu <98572357+chobkyu@users.noreply.github.com>
2023-07-11 15:32:20 +0900

    'FrameLayout'

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
@@ -17,8 +18,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val tab1 = findViewById<Button>(R.id.tab1);
+        val tab2 = findViewById<Button>(R.id.tab2);
+        val content1 = findViewById<TextView>(R.id.content1);
+        val content2 = findViewById<TextView>(R.id.content2);
+        tab1.setOnClickListener {
+             content1.visibility = View.VISIBLE;
+             content2.visibility = View.INVISIBLE
+        }
 
-
+        tab2.setOnClickListener {
+             content1.visibility = View.INVISIBLE;
+             content2.visibility = View.VISIBLE
+        }
     }
 }
 
