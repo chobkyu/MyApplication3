07e97372d86e94bed89ae387e4cd8b04a4536597
chobkyu <98572357+chobkyu@users.noreply.github.com>
2023-07-11 13:19:42 +0900

    'TextView'

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -21,17 +21,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        visibleButton = findViewById(R.id.visibleButton)
-        invisibleButton = findViewById(R.id.invisibleButton)
-        logoImageView = findViewById(R.id.logoImageView)
-
-        visibleButton.setOnClickListener{
-            logoImageView.visibility = View.VISIBLE
-        }
-
-        invisibleButton.setOnClickListener{
-            logoImageView.visibility = View.INVISIBLE
-        }
+//        visibleButton = findViewById(R.id.visibleButton)
+//        invisibleButton = findViewById(R.id.invisibleButton)
+//        logoImageView = findViewById(R.id.logoImageView)
+//
+//        visibleButton.setOnClickListener{
+//            logoImageView.visibility = View.VISIBLE
+//        }
+//
+//        invisibleButton.setOnClickListener{
+//            logoImageView.visibility = View.INVISIBLE
+//        }
 
     }
 }
