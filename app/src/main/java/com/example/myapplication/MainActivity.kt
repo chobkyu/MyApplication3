e1d6638870223382035d9f4ce41e402032681cef
chobkyu <98572357+chobkyu@users.noreply.github.com>
2023-07-12 16:02:51 +0900

    'GridLayout'

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -18,19 +18,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tab1 = findViewById<Button>(R.id.tab1);
-        val tab2 = findViewById<Button>(R.id.tab2);
-        val content1 = findViewById<TextView>(R.id.content1);
-        val content2 = findViewById<TextView>(R.id.content2);
-        tab1.setOnClickListener {
-             content1.visibility = View.VISIBLE;
-             content2.visibility = View.INVISIBLE
-        }
-
-        tab2.setOnClickListener {
-             content1.visibility = View.INVISIBLE;
-             content2.visibility = View.VISIBLE
-        }
+//        val tab1 = findViewById<Button>(R.id.tab1);
+//        val tab2 = findViewById<Button>(R.id.tab2);
+//        val content1 = findViewById<TextView>(R.id.content1);
+//        val content2 = findViewById<TextView>(R.id.content2);
+//        tab1.setOnClickListener {
+//             content1.visibility = View.VISIBLE;
+//             content2.visibility = View.INVISIBLE
+//        }
+//
+//        tab2.setOnClickListener {
+//             content1.visibility = View.INVISIBLE;
+//             content2.visibility = View.VISIBLE
+//        }
     }
 }
 
